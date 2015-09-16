496e268bf24d68c380e67475ffebcd62c9f762bf
unknown <alacey@TX717-5030FQK.us.cbre.net>
2015-09-16 16:19:33 -0500

    Added Elasticsearch connector that retrieves data

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # elasticsearch-tableau-connector
 Tableau Web Data Connector for Elasticsearch
+
+# Overview
+
+This is an instance of a Tableau Web Data Connector for Elasticsearch.  
+
+# Configuration
+- You must enable CORS support in your Elasticsearch server.  Set the following setting in `elasticsearch.yml`:
+
+```yaml
+http.cors.enabled = true
+```
--- index.js
+++ index.js
@@ -1,20 +1,17 @@
 var express = require('express');
 var app = express();
 
-console.log('New express app instance: ', app);
-
-app.get('/elasticsearch-connector', function (req, res) {
-  res.send('Hello World!');
-});
-
+// Simple routes to serve up the needed components installed via bower
+// and the connector HTML file itself served from base path of the web server
 app.use('/jquery', express.static(__dirname + '/bower_components/jquery/dist'));
+app.use('/bootstrap', express.static(__dirname + '/bower_components/bootstrap/dist'));
+app.use('/lodash', express.static(__dirname + '/bower_components/lodash'));
+app.use('/resources', express.static(__dirname + '/resources'));
 app.use(express.static(__dirname + '/connector'));
-
-console.log('Starting server on port 3000');
 
 var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
 
-  console.log('Example app listening at http://%s:%s', host, port);
+  console.log('Elasticsearch Tableau Web Data connector server listening at http://%s:%s', host, port);
 });
