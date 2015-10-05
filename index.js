7081fd0126f626a580989eec52336f274c44e554
Adam Lacey <adam.lacey@cbre.com>
2015-10-05 16:26:32 -0500

    Never cache the connector page, also correctly manage state between phases by storing/retreiving from tableau connectionData

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ var app = express();
 // Serve everything from the same flat folder in the base web server path
 app.use(express.static(__dirname + '/public'));
 
+app.disable('etag');
+
 var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
