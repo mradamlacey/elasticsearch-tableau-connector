92650369db29624532ff405a3ee88b77e011e413
Albert Franzi <albert.franzi@schibsted.com>
2016-05-31 12:35:41 +0200

    [AWS] - Solve compatibility with aws elasticsearh 1.5.2

--- connector/elasticsearch-connector.js
+++ connector/elasticsearch-connector.js
@@ -492,6 +492,8 @@
              data: JSON.stringify(requestData),
              dataType: 'json',
              beforeSend: function (xhr) {
+                xhr.setRequestHeader('Accept', null);
+                xhr.setRequestHeader('Content-Type', 'application/json');
                  if (connectionData.elasticsearchAuthenticate && tableau.username) {
                      xhr.setRequestHeader("Authorization", "Basic " +
                          btoa(tableau.username + ":" + tableau.password));
@@ -499,11 +501,8 @@
 
              },
              success: function (data) {
-
                  clearError();
-
                  var result = processSearchResults(data);
-
                  cb(null, result.scrollId);
              },
              error: function (xhr, ajaxOptions, err) {
@@ -524,20 +523,16 @@
              return;
          }
 
-         var connectionUrl = connectionData.elasticsearchUrl + '/_search/scroll';
-
-         var requestData = {
-             scroll: '5m',
-             scroll_id: scrollId
-         };
+         var connectionUrl = connectionData.elasticsearchUrl + '/_search/scroll?scroll=5m&scroll_id='+scrollId;
 
          var xhr = $.ajax({
              url: connectionUrl,
              method: 'POST',
              processData: false,
-             data: JSON.stringify(requestData),
              dataType: 'json',
              beforeSend: function (xhr) {
+                xhr.setRequestHeader('Accept', null);
+                xhr.setRequestHeader('Content-Type', 'application/json')
                  if (connectionData.elasticsearchAuthenticate && tableau.username) {
                      xhr.setRequestHeader("Authorization", "Basic " +
                          btoa(tableau.username + ":" + tableau.password));
