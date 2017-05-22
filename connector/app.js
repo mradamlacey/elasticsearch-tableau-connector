0d9628bb898a6a9e547555e1086bd9b0f75dc8c3
Adam Lacey <adam.lacey@gmail.com>
2017-05-21 23:59:13 -0500

    Fix URL validation where host ends with digits

--- connector/app.js
+++ connector/app.js
@@ -615,7 +615,7 @@ var app = (function () {
 
     var isValidUrl = function (str) {
         var pattern = new RegExp('^(https?:\/\/)?' + // protocol
-            '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\.)+[a-z]{2,}|' + // domain name
+            '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\.)+[a-z\\d]{2,}|' + // domain name
             '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
             '(\\:\\d+)?(\/[-a-z\\d%_.~+]*)*' + // port and path
             '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
