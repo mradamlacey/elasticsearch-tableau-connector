c8aca3ffb2847976d5d0406c9df23f58371460b3
Maxime L <maximeloizeau@gmail.com>
2017-07-07 12:41:32 +0100

    Use the safe field name earlier for date fields
    For names that are converted to a safe name, the item is skipped because it doesn't match.

--- connector/elasticsearch-connector.js
+++ connector/elasticsearch-connector.js
@@ -896,24 +896,23 @@ var elasticsearchConnector = (function () {
 
                 // Copy over any formatted value to the source object
                 _.each(connectionData.dateFields, function (field) {
-
-                    if (!item[field]) {
+                    fieldName = toSafeTableauFieldName(field);
+                    
+		    if (!item[fieldName]) {
                         return;
                     }
 
-                    if( !_.isString(item[field])){
+                    if( !_.isString(item[fieldName])){
                         return;
                     }
 
                     val = null;
-                    if(_.isArray(item[field])){
-                        val = item[field][0]
+                    if(_.isArray(item[fieldName])){
+                        val = item[fieldName][0]
                     }
                     else{
-                        val = item[field]
+                        val = item[fieldName]
                     }
-                    
-                    fieldName = toSafeTableauFieldName(field);
                     
                     // convert dateField to String before calling .replace() on it
 				    val = val + ''; 
